c5d3a65452ab4954245b0b10f7bbc4f8e7c8ae36
prianto1995 <96390929+prianto1995@users.noreply.github.com>
2021-12-20 10:35:41 +0700

    Change Firebase

--- src/firebase.js
+++ src/firebase.js
@@ -2,13 +2,13 @@ import firebase from 'firebase/compat/app';
 import "firebase/compat/database"
 
 const firebaseConfig = {
-    apiKey: "AIzaSyA9RCcK3n2kqkxxa-MX2gNM0z7AVR1OsOs",
-    authDomain: "react-contact-2754d.firebaseapp.com",
-    databaseURL: "https://react-contact-2754d-default-rtdb.asia-southeast1.firebasedatabase.app",
-    projectId: "react-contact-2754d",
-    storageBucket: "react-contact-2754d.appspot.com",
-    messagingSenderId: "1001757185759",
-    appId: "1:1001757185759:web:e29c7ec7d954cc927dbb6a"
+  apiKey: "AIzaSyDuPn81g-Rw8xZrPoelFfFBUkKiRPtD6oA",
+  authDomain: "technikaltest.firebaseapp.com",
+  databaseURL: "https://technikaltest-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "technikaltest",
+  storageBucket: "technikaltest.appspot.com",
+  messagingSenderId: "678175034948",
+  appId: "1:678175034948:web:7bc7dff04f6ff719f27611",
 };
 
 const fireDb = firebase.initializeApp(firebaseConfig);
